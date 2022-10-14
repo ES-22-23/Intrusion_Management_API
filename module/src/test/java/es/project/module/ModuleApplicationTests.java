c219c554668d48aedaeaacfe9340c2ab43388ea3
Vicente <vicente.costa@ua.pt>
2022-10-14 12:00:28 +0100

    spring inicialzr

---
+++ module/src/test/java/es/project/module/ModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package es.project.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
