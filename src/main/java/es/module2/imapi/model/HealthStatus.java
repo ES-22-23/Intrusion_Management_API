12a790a3e35a42ae94a9b49b1dc2a0d0328e860b
goncalo-machado <goncalofmachado01@gmail.com>
2022-12-18 23:01:02 +0000

    fix healthStatus

--- src/main/java/es/module2/imapi/model/HealthStatus.java
+++ src/main/java/es/module2/imapi/model/HealthStatus.java
@@ -1,11 +1,15 @@
 package es.module2.imapi.model;
 
-import lombok.Data;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
 
 @Data
 public class HealthStatus {
 
+    @JsonProperty("isHealthy")
     private final boolean isHealthy;
     private final List<?> additionalProperties;
 
